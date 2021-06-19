2cc8fdf3739048825674dacac5b083e6edc27085
Lindsay MacDonald <linzmacd@gmail.com>
2021-06-18 23:12:59 -0700

    Updating filters

--- static/js/app.js
+++ static/js/app.js
@@ -82,7 +82,6 @@ function filterTable() {
 
 // Attach an event to listen for changes to each filter
 d3.selectAll("select").on("change", updateFilters);
-d3.selectAll("input").on("change", updateFilters);
 
 // Build the table when the page loads
 buildTable(tableData);
